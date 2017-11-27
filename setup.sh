600b56cf0538a129c788f1dabdf4af488e9cb9d0
jb <jeanb.rocher@gmail.com>
2017-11-27 19:24:20 +0100

    add lamp script tigger from main script ; lamp.sh handle different php versions

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 cat setup.txt
 echo "installing lamp"
-# ./lamp/lamp/#!/bin/sh
+./lamp/lamp/#!/bin/sh
 export php_version
 php_version=`php -r \@phpinfo\(\)\; | grep -o '[0-9]\.[0-9]' -m 1`
 echo "retrevining php version for future use"
 echo "version detected:$php_version"
 
 echo "isntalling additionnal php extensions"
-# ./lamp/php.sh
+./lamp/php.sh
